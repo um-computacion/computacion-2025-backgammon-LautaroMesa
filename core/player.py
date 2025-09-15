483ab0fd5f5f7ff12163aab46583a7895b184e76
Lautaro Mesa <la0.mesa@alumno.um.edu.ar>
2025-09-15 22:43:29 +0000

    Arreglos y avances

--- core/player.py
+++ core/player.py
@@ -5,14 +5,24 @@ class Player:      # Caracteristicas de un jugador de Backgammon.
         #Inicializa un jugador de Backgammon.
         #para__nombre__: Nombre del jugador.
         #para__color__: Color de las fichas del jugador ('blanco' o 'negro').
-        
+
+        if __color__ not in ['blanco', 'negro']:
+            raise ValueError("El color debe ser 'blanco' o 'negro'.")
         self.__nombre__ =__nombre__ 
         self.__color__ = __color__
+        self.__fichas_en_barra__ = 0
+        self.__fichas_fuera__ = 0
+        self.__es_turno__ = False
 
-    def __str__(self):
-       
-        #Devuelve una representación en texto del jugador.
-        #return: Cadena con nombre y color.
+    def obtener_nombre(self):
+        #Devuelve el nombre del jugador.
+        #return: Nombre del jugador.
         
-        return f"{self.__nombre__} ({self.__color__})"       
-       
+        return self.__nombre__
+    
+    def obtener_color(self):
+        #Devuelve el color de las fichas del jugador.
+        #return: Color del jugador ('blanco' o 'negro').
+        return self.__color__
+     
+    
